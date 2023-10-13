ead3c80bdd5289870cff63e2673b061505d5c7e9
TDWolff <torin.dean.wolff@gmail.com>
2023-10-13 09:22:20 -0700

    more mods

--- api/encode.py
+++ api/encode.py
@@ -2,7 +2,9 @@ from flask import Blueprint, jsonify, request  # Import 'request' from 'flask'
 from flask_restful import Api, Resource
 import requests
 import random
+import csv
 from model.encode import *
+import json
 
 encode_api = Blueprint('encode_api', __name__, url_prefix='/api/encode')
 api = Api(encode_api)
@@ -10,21 +12,26 @@ api = Api(encode_api)
 class EncodeAPI():
     class _Read(Resource):
         def get(self):
-            return jsonify(getResponses())
+            with open('./data/userresponses.csv', mode='r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                responses = [{'response': row['response']} for row in csv_reader]
+            return jsonify(responses)
 
     class _Write(Resource):
         def post(self):
             try:
                 data = request.get_json()
-                response = data.get('userInput')  # Correctly access the 'userInput' key
-                addResponse(response)
-                respond_list.append(response)  # Add the response to the respond_list list
-                return {'response': 'success', 'respond_list': respond_list}  # Return a JSON response indicating success and the updated respond_list
+                response = data.get('userInput')
+                with open('./data/userresponses.csv', mode='a', newline='') as csv_file:
+                    fieldnames = ['response']
+                    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                    writer.writerow({'response': response})
+                return {'response': 'success'}
             except Exception as e:
                 print('Error adding response:', e)
-                return {'response': 'error', 'message': str(e)}  # Return a JSON response with an error message
+                return {'response': 'error', 'message': str(e)}
 
-api.add_resource(EncodeAPI._Read, '/')  # Add the correct resource classes
+api.add_resource(EncodeAPI._Read, '/')
 api.add_resource(EncodeAPI._Write, '/write')
 
 
--- model/encode.py
+++ model/encode.py
@@ -5,8 +5,10 @@ respond_list = ["Hello", "Hi", "Hey",]
 respond_data = []
 
 def initEncode():
-    for item in respond_list:
-        respond_data.append({"response": item})
+    with open('./data/userresponses.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            respond_data.append({"response": row["response"]})
 
 def getResponses():
     return respond_data
@@ -20,25 +22,4 @@ def addResponse(response):
         return respond_data[-1]
 
 # Call initEncode to initialize the responses
-initEncode()
-# models/encode.py
-
-def initEncode():
-    with open('./data/userresponses.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            respond_data.append({"response": row["response"]})
-
-def getResponses():
-    return respond_data
-
-def addResponse(response):
-    with open('userresponses.csv', mode='a') as csv_file:
-        fieldnames = ['response']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writerow({'response': response})
-        respond_data.append({"response": response})
-        return respond_data[-1]
-
-respond_data = []
 initEncode()
