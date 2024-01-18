7f3798184816d0767cec8b0df646d3bb3092d409
TDWolff <torin.dean.wolff@gmail.com>
2024-01-18 11:25:56 -0800

    pushing modifications to awsNAVT, user login, registration, and delete

--- api/user.py
+++ api/user.py
@@ -1,87 +1,33 @@
-import json
-from flask import Blueprint, request, jsonify
-from flask_restful import Api, Resource # used for REST API building
-from datetime import datetime
+import sqlite3
 
-from model.users import User
 
-user_api = Blueprint('user_api', __name__,
-                   url_prefix='/api/users')
 
-# API docs https://flask-restful.readthedocs.io/en/latest/api.html
-api = Api(user_api)
+db_path = 'volumes/db/userdata.db'
+print("db_path: " + db_path)
 
-class UserAPI:        
-    class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
-        def post(self): # Create method
-            ''' Read data for json body '''
-            body = request.get_json()
-            
-            ''' Avoid garbage in, error checking '''
-            # validate name
-            name = body.get('name')
-            if name is None or len(name) < 2:
-                return {'message': f'Name is missing, or is less than 2 characters'}, 400
-            # validate uid
-            uid = body.get('uid')
-            if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
-            # look for password and dob
-            password = body.get('password')
-            dob = body.get('dob')
+conn = sqlite3.connect(db_path, check_same_thread=False)
+cursor = conn.cursor()
 
-            ''' #1: Key code block, setup USER OBJECT '''
-            uo = User(name=name, 
-                      uid=uid)
-            
-            ''' Additional garbage error checking '''
-            # set password if provided
-            if password is not None:
-                uo.set_password(password)
-            # convert to date type
-            if dob is not None:
-                try:
-                    uo.dob = datetime.strptime(dob, '%Y-%m-%d').date()
-                except:
-                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 400
-            
-            ''' #2: Key Code block to add user to database '''
-            # create user in database
-            user = uo.create()
-            # success returns json of user
-            if user:
-                return jsonify(user.read())
-            # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users(
+               username TEXT NOT NULL,
+               password TEXT NOT NULL,
+    )
+''')
 
-        def get(self): # Read Method
-            users = User.query.all()    # read/extract all users from database
-            json_ready = [user.read() for user in users]  # prepare output in json
-            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
-    
-    class _Security(Resource):
 
-        def post(self):
-            ''' Read data for json body '''
-            body = request.get_json()
-            
-            ''' Get Data '''
-            uid = body.get('uid')
-            if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
-            password = body.get('password')
-            
-            ''' Find user '''
-            user = User.query.filter_by(_uid=uid).first()
-            if user is None or not user.is_password(password):
-                return {'message': f"Invalid user id or password"}, 400
-            
-            ''' authenticated user '''
-            return jsonify(user.read())
+def visualize_data():
+    conn = sqlite3.connect(db_path, check_same_thread=False)
+    cursor = conn.cursor()
 
-            
+    cursor.execute('SELECT * FROM users')
+    data = cursor.fetchall()
+    print(data)
+    result = []
 
-    # building RESTapi endpoint
-    api.add_resource(_CRUD, '/')
-    api.add_resource(_Security, '/authenticate')
-    
+    for users in data:
+        result = []
+        for user in data:
+            result.append(f"Username: {user[0]}, Password: {user[1]}")
+    conn.close()
+    return result
